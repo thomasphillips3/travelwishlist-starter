257794ea368737db4511442bae6798287e262dbf
Thomas Phillips <thomasphillips3@gmail.com>
2017-11-12 23:04:14 -0800

    FAB transition is much smoother

--- app/src/main/java/com/raywenderlich/android/travelwishlist/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/travelwishlist/MainActivity.kt
@@ -9,8 +9,10 @@ import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.Window
 import android.widget.ImageView
 import android.widget.LinearLayout
+import com.raywenderlich.android.travelwishlist.R.id.toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,13 +25,23 @@ class MainActivity : AppCompatActivity() {
     private val onItemClickListener = object :
             TravelListAdapter.OnItemClickListener {
         override fun onItemClick(view: View, position: Int) {
-            val intent = DetailActivity.newIntent(this@MainActivity, position)
+            val transitionIntent = DetailActivity.newIntent(this@MainActivity, position)
             val placeImage = view.findViewById<ImageView>(R.id.placeImage)
             val placeNameHolder = view.findViewById<LinearLayout>(R.id.placeNameHolder)
+            val navigationBar = findViewById<View>(android.R.id.navigationBarBackground)
+            val statusBar = findViewById<View>(android.R.id.statusBarBackground)
             val imagePair = Pair.create(placeImage as View, "tImage")
             val holderPair = Pair.create(placeNameHolder as View, "tNameHolder")
+            val navPair = Pair.create(navigationBar, Window.NAVIGATION_BAR_BACKGROUND_TRANSITION_NAME)
+            val statusPair = Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME)
+            val toolbarPair = Pair.create(toolbar as View, "tActionBar")
+            val pairs = mutableListOf(imagePair, holderPair, statusPair, toolbarPair)
+            if (navigationBar != null && navPair != null) {
+                pairs += navPair
+            }
             val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this@MainActivity, imagePair, holderPair)
-            ActivityCompat.startActivity(this@MainActivity, intent, options.toBundle())
+            ActivityOptionsCompat.makeSceneTransitionAnimation(this@MainActivity, *pairs.toTypedArray())
+            ActivityCompat.startActivity(this@MainActivity, transitionIntent, options.toBundle())
         }
     }
 
@@ -43,10 +55,14 @@ class MainActivity : AppCompatActivity() {
         adapter = TravelListAdapter(this)
         list.adapter = adapter
         adapter.setOnItemClickListener(onItemClickListener)
+        setUpActionBar()
     }
 
     private fun setUpActionBar() {
-
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.elevation = 7.0f
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
